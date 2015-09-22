34d75f2c7ed2c5b94b1d567d3af1a80d192eb606
Richard Hamilton <richardgregoryhamilton@gmail.com>
2015-09-22 13:37:12 -0500

    Add test cases

--- spec/models/person_spec.rb
+++ spec/models/person_spec.rb
@@ -1,5 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe Person, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+	let(:person) do
+		Person.new(first_name: 'Alice', last_name: 'Smith')
+	end
+	
+	it 'is valid' do
+		expect(person).to be_valid
+	end
+	
+	it 'is invalid without a first name' do
+		person.first_name = nil
+		expect(person).to_not be_valid
+	end
+	
+	it 'is invalid without a last name' do
+		person.last_name = nil
+		expect(person).to_not be_valid
+	end
+	
+	it 'updates the requested person' do
+		person = Person.create!(valid_attributes)
+		put :update, { :id => person.to_param, :person => new_attributes }, valid_session
+		person.reload
+		expect(person.first_name).to eq('NewFirstName')
+		expect(person.last_name).to eq('NewLastName')
+	end
+	
 end
